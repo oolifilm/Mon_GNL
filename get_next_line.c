577fdd2815ab5455a8d6f12063f283f16f4c638d
lele <160538347+oolifilm@users.noreply.github.com>
2024-06-17 13:15:38 +0200

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -5,9 +5,90 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: leaugust <leaugust@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/13 15:36:34 by leaugust          #+#    #+#             */
-/*   Updated: 2024/06/13 15:38:27 by leaugust         ###   ########.fr       */
+/*   Created: 2024/06/16 22:24:30 by leaugust          #+#    #+#             */
+/*   Updated: 2024/06/17 12:25:17 by leaugust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+void	test(void)
+{
+	get_next_line(-42);
+}
+
+char	*fill_line_buffer(int fd, char *stash, char *buffer)
+{
+	int		bytes_read;
+	char	*temp;
+
+	bytes_read = 1;
+	while (bytes_read > 0)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(stash);
+			return (NULL);
+		}
+		else if (bytes_read == 0)
+			break ;
+		buffer[bytes_read] = '\0';
+		if (!stash)
+			stash = ft_strdup("");
+		temp = stash;
+		stash = ft_strjoin(temp, buffer);
+		free(temp);
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	return (stash);
+}
+
+char	*set_line(char *line_buffer)
+{
+	size_t	i;
+	char	*stash;
+
+	i = 0;
+	while (line_buffer[i] != '\n' && line_buffer[i])
+		i++;
+	if (line_buffer[i] == '\0')
+		return (NULL);
+	stash = ft_substr(line_buffer, i + 1, ft_strlen(line_buffer) - (i + 1));
+	if (*stash == '\0')
+	{
+		free(stash);
+		stash = NULL;
+	}
+	line_buffer[i + 1] = '\0';
+	return (stash);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stash;
+	char		*line;
+	char		*buffer;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+	{
+		if (stash)
+			free(stash);
+		return (NULL);
+	}
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer || read(fd, buffer, 0) < 0)
+	{
+		free(stash);
+		free(buffer);
+		stash = NULL;
+		return (NULL);
+	}
+	line = fill_line_buffer(fd, stash, buffer);
+	free(buffer);
+	if (!line)
+		return (NULL);
+	stash = set_line(line);
+	return (line);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -5,16 +5,32 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: leaugust <leaugust@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/13 15:36:32 by leaugust          #+#    #+#             */
-/*   Updated: 2024/06/13 15:38:15 by leaugust         ###   ########.fr       */
+/*   Created: 2024/06/16 22:25:00 by leaugust          #+#    #+#             */
+/*   Updated: 2024/06/17 12:09:44 by leaugust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifdef GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+# include <fcntl.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 char	*get_next_line(int fd);
+char	*set_line(char *line_buffer);
+char	*fill_line_buffer(int fd, char *left_c, char *buffer);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *s1);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+void	test(void) __attribute__((destructor));
 
 #endif
